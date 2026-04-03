33ae6fef91c565aa15b881d7bce3ae6691a7f8d1
biancaciriloads <biancacirilo.ads@gmail.com>
2026-04-03 17:04:45 -0300

    fonte configurada

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        graphik: ['Graphik', 'sans-serif'],
+        'graphik-bold': ['Graphik-bold', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}

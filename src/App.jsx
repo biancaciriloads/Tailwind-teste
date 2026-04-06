0d808a5b3abdd27f112c3eb39adffeb32d0ce2d0
biancaciriloads <biancacirilo.ads@gmail.com>
2026-04-06 01:48:57 -0300

    Curso Tailwind Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { CalendarFeature } from './Components/CalendarFeature'
+import { Testimonials } from './Components/Testimonials'
 
 function App() {
   return (
     <div className='bg-dark-background'>
+       <Testimonials />
       <CalendarFeature />
     </div>
   )
